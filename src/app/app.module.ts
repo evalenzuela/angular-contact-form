f33f204ed77ffa9c5b2ae8ac1d7cb586721ca75d
Eduardo Valenzuela <evalenzuela@catarxis.com>
2017-07-22 11:24:16 -0500

    Creating thanks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { ThanksComponent } from './thanks/thanks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    ThanksComponent
   ],
   imports: [
     BrowserModule
